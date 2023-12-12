ae8fa72a35f507c290cadfc06c0aad698eba9208
AvaneenP <avaneen@gmail.com>
2023-12-12 16:59:37 -0500

    Updated neural network steering file - has updated LIDAR code

--- final/final/steering_NN.py
+++ final/final/steering_NN.py
@@ -1,22 +1,23 @@
 # This file imports the model weights & biases from a .pt file
 # Uses the imported NN to steer the robot
-# Also uses LIDAR to stop the robot when it is too close to an obstacle
+# MAKE SURE TO SPLIT THE IMAGE, get 2 predictions and then average the steering outputs
 
 import rclpy
 from rclpy.node import Node
-
-import os
-import pandas as pd
-import numpy as np
-import sys
-from PIL import Image
-import cv2
-from cv_bridge import CvBridge
-import sensor_msgs.msg
 from geometry_msgs.msg import Twist
-import torch
+import sensor_msgs.msg
+from cv_bridge import CvBridge
+import cv2
+import numpy as np
+import os
+from PIL import Image
+import pandas as pd
+import sys
 from torchvision.transforms import Compose, ToTensor
+# sys.path.append("../models")
 from .DAVE2pytorch import DAVE2v3
+import torch
+
 
 class Steering_NN(Node):
     def __init__(self):
@@ -24,20 +25,21 @@ class Steering_NN(Node):
         super().__init__('steering_NN')
 
         # Load model weights & bias
-        input_shape = (640, 360) # Input shape of the image
+        input_shape = (640, 360)
         self.model = DAVE2v3(input_shape=input_shape)
-        # Change the path below based on where your .pt file is located on your robot
-        self.model.load_state_dict(torch.load('/home/husarion/ros2_ws/src/final/final/model_30k_11epoch.pt', map_location=torch.device('cpu')))
+        self.model.load_state_dict(torch.load('/home/husarion/ros2_ws/src/final/final/old_data_360x640.pt', map_location=torch.device('cpu')))
+        # self.model = torch.load('/home/husarion/ros2_ws/src/final/final/model-DAVE2v3-135x240-lr0.001-50epoch-64batch-lossMSE-7Ksamples-INDUSTRIALandHIROCHIandUTAH-noiseflipblur-best.pt')
         self.model.eval()
 
-        # Creating velocity publisher and LIDAR 
         self.publisher_vel = self.create_publisher(Twist, '/cmd_vel', 1)
         self.image_subscription = self.create_subscription(sensor_msgs.msg.Image, '/image_raw', self.image_callback, 10)
-        self.lidar_subscription = self.create_subscription(sensor_msgs.msg.LaserScan, '/scan', self.lidar_callback, 10)
         print("lidar was subscribed to")
+        self.lidar_subscription = self.create_subscription(sensor_msgs.msg.LaserScan, '/scan', self.lidar_callback, 10)
 
-        self.min_pause_distance = 0.33 # If an obstacle is within 0.33 meters of the front of the robot, we stop
+        self.min_pause_distance = 0.35
         self.obstacle_closeby = False
+        self.postObstacle_counterTurn = 0
+        self.sign = 1
 
         self.bridge = CvBridge()
         self.bridged_image = None
@@ -47,7 +49,8 @@ class Steering_NN(Node):
         self.right_image = None
 
         self.vel = Twist()
-        self.vel.linear.x = 0.3 # Constant linear speed of the robot
+        self.max_speed = 0.3
+        self.vel.linear.x = self.max_speed
         self.vel.angular.z = 0.0
 
         # Timer callback to publish the velocities at that moment
@@ -57,22 +60,24 @@ class Steering_NN(Node):
     def timer_callback(self):
         
         #UNSPLIT IMAGE
-        # If an image hasn't been received by the callback or the robot is too close to an obstacle, do not use the NN to infer an angular velocity
-
-        if self.unsplit_image == None or self.obstacle_closeby:
+        if self.unsplit_image == None or self.obstacle_closeby or self.postObstacle_counterTurn > 0:
             return
-        
-        # Model inference to output angular velocity prediction
         transformed_image = Compose([ToTensor()])(self.unsplit_image)
         input_image = transformed_image.unsqueeze(0)
         self.vel.angular.z = self.model(input_image).item()
-
+        print('NEURAL NETWORK TURN')
+        # make neural network turns more drastic
+        if self.vel.angular.z < 0:
+            self.vel.angular.z = self.vel.angular.z 
+        if self.vel.angular.z > 0:
+            self.vel.angular.z = self.vel.angular.z * 3
         self.publisher_vel.publish(self.vel)
         print(self.vel.angular.z)
 
 
 
-        # SPLIT IMAGE CODE (used when model training and deployment used the left and right images from the ZED2 camera)
+        # Model inference to output angular velocity prediction
+        #SPLIT IMAGE CODE
         '''
         if self.left_image == None or self.right_image == None or self.obstacle_closeby:
             return
@@ -98,11 +103,14 @@ class Steering_NN(Node):
         '''
     
     def image_callback(self, msg):
+        #might need to do some reversing, not too sure yet
         self.bridged_image = self.bridge.imgmsg_to_cv2(msg, desired_encoding = 'passthrough')
+        # cv2.imshow('', self.bridged_image)
+        # print("Should get image here")
         img = Image.fromarray(self.bridged_image)
         self.unsplit_image = img.resize((640,360))
 
-        #SPLIT IMAGE CODE (used when model training and deployment used the left and right images from the ZED2 camera)
+        #SPLIT IMAGE CODE
         '''
         img = Image.fromarray(self.bridged_image)
         width, height = img.size
@@ -113,12 +121,91 @@ class Steering_NN(Node):
         self.right_image = self.right_image.resize((320, 360))
         '''
 
+        # print("Left image size:", self.left_image.size)
+        # print("Right Image size:", self.right_image.size)
     
     def lidar_callback(self, msg):
+        #the lidar scans right infront of it as index 0
         lidar_ranges = msg.ranges
+        self.vel.linear.x = self.max_speed
+
+        """
+        STEERING CORRECTION BUBBLE
+        Values go from 0 to 1800 (CCW)
+        """
+        left_distances = lidar_ranges[0:600]
+        right_distances = lidar_ranges[1200:1800]
+
+
+        left_total = 0
+        right_total = 0
+        left_close_count = 0
+        right_close_count = 0
+        bubble_radius = .4
+        threshold = len(left_distances) * bubble_radius
+
+        for dist in left_distances:
+            if dist == float('inf'):
+                left_total += 0.15
+                left_close_count += 1
+            elif dist > bubble_radius:
+                left_total += bubble_radius
+            else:
+                left_close_count += 1
+                left_total += dist
+        
+        for dist in right_distances:
+            if dist == float('inf'):
+                right_total += 0.15
+                right_close_count += 1
+            elif dist > bubble_radius:
+                right_total += bubble_radius
+            else:
+                right_close_count += 1
+                right_total += dist
+
+        if self.postObstacle_counterTurn > 0:
+            self.vel.angular.z = self.sign * -0.4
+            self.postObstacle_counterTurn -= 1
+
+        #if left_total < threshold and left_total < right_total:
+        if left_close_count > len(left_distances) // 3:
+            self.unsplit_image = None
+            self.obstacle_closeby = True
+            self.vel.angular.z = (left_total - threshold) * 0.005
+
+        #elif right_total < threshold:
+        elif right_close_count > len(right_distances) // 3:
+            self.unsplit_image = None
+            self.obstacle_closeby = True
+            self.vel.angular.z = (threshold - right_total) * 0.005
+        else:
+            if self.obstacle_closeby == True:
+                self.postObstacle_counterTurn = 2
+                self.sign = (self.vel.angular.z / abs(self.vel.angular.z))
+                self.vel.angular.z = self.sign * -0.4
+            print("bubble is not in range")
+            self.obstacle_closeby = False
+
+        
+
+        if self.obstacle_closeby or self.postObstacle_counterTurn > 0:
+            if not self.obstacle_closeby:
+                print('COUNTER TURN')
+            else:
+                print('BUBBLE TURN')
+            self.publisher_vel.publish(self.vel)
+            print(self.vel.angular.z)
+
+
+        """
+        STOPPING IN FRONT OF OBSTACLES
+        This should also turn away from the direction that had the points closest to it
+        """
+        
         front_indicies = len(lidar_ranges) // 6 #~60 degrees of points
 
-        #Take the ranges from the lidar from the front
+        # might be really slow
         front_ranges = lidar_ranges[-1 - front_indicies: -1] #the left 60 degrees from the middle 
         front_ranges.extend(lidar_ranges[0:front_indicies]) # the right 60 degrees from the middle
         
@@ -126,18 +213,17 @@ class Steering_NN(Node):
         for curr_range in front_ranges:
             if curr_range < self.min_pause_distance or curr_range == float('inf'):
                 close_counter += 1
-        if close_counter > len(front_ranges) // 2.5:
-            # If 40% of the points in front of the robot are within the stopping distance, stop the robot
+        if close_counter > len(front_ranges) // 3:
             self.obstacle_closeby = True
-            self.publisher_vel.publish(Twist()) # Stops the robot instantly
             #if this becomes false, we want the nn to predict on a new image, not some old stored one
-            self.left_image = None
-            self.right_image = None
             self.unsplit_image = None
+            self.vel.linear.x = -1 * self.max_speed
+            self.publisher_vel.publish(self.vel)
+
+
             print("TOO CLOSE!!!")
         else:
             self.obstacle_closeby = False
-            
 
 
 
